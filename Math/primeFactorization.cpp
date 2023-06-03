9aa550e534d44826bc9cf6e6b7119fe2851e0596
osamax01 <shkukanios@gmail.com>
2023-06-03 13:17:01 +0300

    delete unrelated code from PrimeFactorization

--- Math/primeFactorization.cpp
+++ Math/primeFactorization.cpp
@@ -3,11 +3,8 @@
 using namespace std;
 typedef long long ll;
 
-vector<ll> primes;
-set<int> factors;
-int n;
-
-void PrimeFactorization(ll x) {
+vector<ll> factorize(ll x) {
+    vector<ll> primes;
     while (x % 2 == 0) {
         primes.push_back(2);
         x /= 2;
@@ -21,32 +18,5 @@ void PrimeFactorization(ll x) {
     }
 
     if (x > 2) primes.push_back(x);
-}
-
-void rec(int idx, ll mul) {
-    if (idx == n) {
-        factors.emplace(mul);
-        return;
-    }
-    rec(idx + 1, mul);
-    rec(idx + 1, mul * primes[idx]);
-}
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    ll x;
-    cin >> x;
-    PrimeFactorization(x);
-    n = primes.size();
-    rec(0, 1);
-
-    cout << "Primes : ";
-    for (auto it : primes) {
-        cout << it << " ";
-    }
-    cout << "\nFactors : ";
-    for (auto it : factors) {
-        cout << it << " ";
-    }
+    return primes;
 }
