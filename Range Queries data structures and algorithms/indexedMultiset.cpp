19de5fe850e609d04d9435a80c56057d15155b7a
osamax01 <shkukanios@gmail.com>
2023-06-03 13:35:46 +0300

    add functions describtion in indexedMultiset.cpp

--- Range Queries data structures and algorithms/indexedMultiset.cpp
+++ Range Queries data structures and algorithms/indexedMultiset.cpp
@@ -7,6 +7,12 @@ using namespace __gnu_pbds;
 typedef long long ll;
 template <typename T> using indexed_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
+/*
+ find_by_order() and order_of_key().
+ The first returns an iterator to the k-th largest element (counting from zero),
+ the second — the number of items in a set that are strictly smaller than our item.
+*/
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
